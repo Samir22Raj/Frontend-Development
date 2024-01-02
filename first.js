4c9fb0800d523e1ed2946c96d6c22a10a1c323ac
Samir Rajkarnikar <rajkarnikar.samir@webexpertsnepal.com>
2024-01-03 00:03:51 +0545

    first

--- first.js
+++ first.js
@@ -100,7 +100,7 @@ console.log('2==="2": '+(2==="2"));
 
 //Hoisting
 console.log("HOisting");
-let weather = "Sunny";
+lmainet weather = "Sunny";
 function changeWeather(){
     weather = "Rainy";
     console.log("Changed Weather to: "+weather);
