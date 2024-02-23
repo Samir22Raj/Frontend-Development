f229e95fc156a58ca7b90c9bc546c0d30ee951b0
Samir Rajkarnikar <samirrajkarnikar9@gmail.com>
2024-02-23 10:39:30 +0545

    Tutorial complete

--- Tailwind/Tailwind Tutorial/tailwind.config.js
+++ Tailwind/Tailwind Tutorial/tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
 	content: ["*"],
 	theme: {
-		extend: {},
+		screens: {
+			xsm: "520px",
+			sm: "640px",
+			md: "768px",
+			lg: "1024px",
+			xl: "1280px",
+			"2xl": "1536px",
+		},
+		extend: {
+			spacing: {
+				13: "3.2rem",
+			},
+			fontSize: {
+				10: ["9rem", { lineHeight: "1.2" }],
+			},
+		},
 	},
 	plugins: [],
 };
