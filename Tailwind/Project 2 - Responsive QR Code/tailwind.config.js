c7aaea136587f1ca3ade1e66edc51e06795ef373
Samir Rajkarnikar <samirrajkarnikar9@gmail.com>
2024-03-06 15:53:45 +0545

    Completed QR code

--- Tailwind/Project 2 - Responsive QR Code/tailwind.config.js
+++ Tailwind/Project 2 - Responsive QR Code/tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
 	content: ["./*.html"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				white: "hsl(0, 0%, 100%)",
+				lightGray: "hsl(212, 45%, 89%)",
+				grayishBlue: "hsl(220, 15%, 55%)",
+				darkBlue: "hsl(218, 44%, 22%)",
+			},
+			fontFamily: {
+				outfit: ["Outfit", "sans-serif"],
+			},
+			screens: {
+				mobile: "375px",
+			},
+		},
 	},
 	plugins: [],
 };
